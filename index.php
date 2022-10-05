bfa8108095765ba532e2264d49c47b052dc1f854
PalComTech <riskydwiansyah220697>
2022-10-05 21:16:38 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$angka1 = 100;
+$angka2 = 100;
+$tambah = $angka1 + $angka2;
+$kurang = $angka1 - $angka2;
+$kali = $angka1 * $angka2;
+$bagi = $angka1 / $angka2;
+echo 
+"angka1=  ".$angka1. "<br>".
+"angka2=  ".$angka2. "<br>".
+"Hasil Tambah = ".$tambah. "<br>".
+"Hasil Kurang = " .$kurang. "<br>".
+"Hasil Kali = " .$kali. "<br>".
+"Hasil Bagi = " .$bagi. "<br>";
+?>
